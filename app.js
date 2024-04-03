2e47ee4427cbd103e92a998a3fb9d8d4cd71660d
Saurabh-Techie <sk2009025@gmail.com>
2024-04-03 21:38:29 +0530

    some js are added

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const input = document.querySelector('#input');
 const add = document.querySelector('#add');
-const list  = document.querySelector('#LI');
+const list  = document.querySelector('#UL');
 
+add.addEventListener("click",()=>{
+    const li = document.createElement('li');
+    li.id = "LI";
+    list.appendChild(li)
+    const span = document.createElement('span')
+    span.innerHTML="X"
+    span.id = SPAN
+    const text = document.createTextNode(input.value);
+    li.appendChild(text)
+    li.appendChild(span)
+    
+})
